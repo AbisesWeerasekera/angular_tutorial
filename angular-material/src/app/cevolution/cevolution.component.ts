c56666b322119da6e364e0dad980ba41da89d9c4
AbisesWeerasekera <52200609+AbisesWeerasekera@users.noreply.github.com>
2021-10-21 02:09:14 +0530

    addedstyle binding

--- angular-material/src/app/cevolution/cevolution.component.ts
+++ angular-material/src/app/cevolution/cevolution.component.ts
@@ -6,7 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cevolution.component.css']
 })
 export class CevolutionComponent implements OnInit {
+  //properties
+  //property binding
   public myId = "textid";
+ //----
+  //class binding
+  public successClass = "text_warning";
+  public hasError = false;
+
+  public messageClass={ //object
+    "text_success" : !this.hasError,
+    "text_warning":this.hasError
+  } 
+//----
+
+//style binding
+public highlight = "orange";
+public title = {
+  color:"brown",
+  fontStyle : "italic"
+}
 
   constructor() { }
 
